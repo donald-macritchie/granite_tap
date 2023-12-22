a785ec88747879111f101843181abffb23e15677
donald-macritchie <>
2023-12-22 14:18:25 +0000

    adjust products.json to include more tahn one categories per product

--- products/admin.py
+++ products/admin.py
@@ -3,13 +3,16 @@ from .models import Product, Category
 
 # Register your models here.
 
+from django.contrib import admin
+from .models import Product, Category
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
         'name',
         'brewery',
         'abv',
-        'category',
+        'display_categories',
         'price',
         'rating',
         'image',
@@ -17,13 +20,16 @@ class ProductAdmin(admin.ModelAdmin):
 
     ordering = ('sku',)
 
+    def display_categories(self, obj):
+        return ', '.join(category.name for category in obj.categories.all())
+
+    display_categories.short_description = 'Categories'
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
     )
-
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -16,8 +16,25 @@ class Category(models.Model):
         return self.friendly_name
 
 
+# class Product(models.Model):
+#     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+#     sku = models.CharField(max_length=254, null=True, blank=True)
+#     name = models.CharField(max_length=254)
+#     brewery = models.CharField(max_length=254)
+#     abv = models.DecimalField(max_digits=6, decimal_places=2)
+#     beer_type = models.CharField(max_length=254)
+#     description = models.TextField()
+#     volume = models.CharField(max_length=254)
+#     price = models.DecimalField(max_digits=6, decimal_places=2)
+#     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+#     image_url = models.URLField(max_length=1024, null=True, blank=True)
+#     image = models.ImageField(null=True, blank=True)
+
+#     def __str__(self):
+#         return self.name
+
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    categories = models.ManyToManyField(Category)  # Change here
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     brewery = models.CharField(max_length=254)
