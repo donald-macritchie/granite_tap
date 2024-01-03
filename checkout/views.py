b959b71e220839fdb355870eec70f530e09c81c3
donald-macritchie <>
2024-01-03 18:25:13 +0000

    added stripe elements and script

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OEY8hKJUkUZDiFC2r5zazrYvy4L2oKRJmViKK9QK2f5IB5UoPWZyDDUwHnsPAcdxvcYF5kSueoZs5utVk63nMF6006dhiS0hS',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
