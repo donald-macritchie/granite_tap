98489503f160377353014dea686ab20243dfb4a0
donald-macritchie <>
2024-01-07 13:18:31 +0000

    add allowed hosts

--- granite_tap/settings.py
+++ granite_tap/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
-import dj_database_url
 if os.path.isfile("env.py"):
     import env
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-donald-macritchie-granit-nf6pbo5vyd.us2.codeanyapp.com',
-                    'granite-tap.herokuapp.com',]
+ALLOWED_HOSTS = ['8000-donald-macritchie-granit-nf6pbo5vyd.us2.codeanyapp.com', 'granite-tap.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -127,13 +126,13 @@ WSGI_APPLICATION = 'granite_tap.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get(DATABASE_URL))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
