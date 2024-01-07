2711b27e6104c9de746aaf1b65ee079101a39b65
donald-macritchie <>
2024-01-07 17:16:51 +0000

    updated stripe wh secret

--- granite_tap/settings.py
+++ granite_tap/settings.py
@@ -28,8 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
 
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-donald-macritchie-granit-nf6pbo5vyd.us2.codeanyapp.com', 'granite-tap-8e0d9d617920.herokuapp.com', 'localhost']
 
