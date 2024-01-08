e64f265a2a4b4d4275f58ea3879403d5e32efa88
donald-macritchie <>
2024-01-08 14:24:14 +0000

    added about section titles and content to database and displayed in template

--- about/views.py
+++ about/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from .models import About, FAQ
 
 def about_page(request):
-    about_content = About.objects.first()
+    about_content = About.objects.all()
     faqs = FAQ.objects.all()
 
-    content {
+    template = 'about/about.html'
+
+    context = {
         'about_content': about_content,
         'faqs': faqs,
     }
-    return render(request, 'pages/about.html', )
+    return render(request, template, context)
