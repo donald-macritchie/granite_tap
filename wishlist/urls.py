ef920f96d95141111f2224378fbf52b293375b41
donald-macritchie <donald.macritchie@outlook.com>
2024-01-09 18:30:29 +0000

    added remove from wishlist functionality

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -6,4 +6,5 @@ app_name = 'wishlist'
 urlpatterns = [
     path('', views.wishlist, name='wishlist'),
     path('add/<int:item_id>/', views.add_to_wishlist, name='add_to_wishlist'),
+    path('remove/<int:item_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Wishlist
 from products.models import Product
 from django.contrib import messages
@@ -11,6 +11,7 @@ def wishlist(request):
         template = 'wishlist/wishlist.html'
         context = {
             'wishlist_items': wishlist_items,
+            
         }
         return render(request, template, context)
     else:
@@ -32,3 +33,22 @@ def add_to_wishlist(request, item_id):
 
     request.session['wishlist'] = wishlist
     return redirect('product_detail', product_id=item_id)
+
+
+def remove_from_wishlist(request, item_id):
+    """ Remove a product from the wishlist """
+    product = get_object_or_404(Product, pk=item_id)
+
+    wishlist = request.session.get('wishlist', [])
+
+    if item_id in wishlist:
+        wishlist.remove(item_id)
+        messages.success(request, f'Removed {product.name} from your wishlist')
+    else:
+        messages.info(request, f'{product.name} is not in your wishlist.')
+
+    request.session['wishlist'] = wishlist
+
+
+    wishlist_url = reverse('wishlist:wishlist')
+    return redirect(wishlist_url)
