679ae7cefcdffc0e7edd3655ba3075c94022bcf6
donald-macritchie <donald.macritchie@outlook.com>
2024-01-09 15:13:16 +0000

    added wishlist view, urls, admin and template

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Wishlist
 
-# Register your models here.
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'date_added')
+
+admin.site.register(Wishlist, WishlistAdmin)
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Wishlist
+from products.models import Product
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+@login_required
+def wishlist(request):
+    wishlist_items = Wishlist.objects.filter(user=request.user)
+
+    template = 'wishlist/wishlist.html'
+
+    context = {
+        'wishlist_items': wishlist_items
+    }
+
+    return render(request, template, context)
+
